751e4a3237af30bdfbad3d539d499bfe0ea886a2
Jermaine Marshall <jermainemarshall@Jermaines-MacBook-Pro.local>
2018-09-09 13:44:19 -0400

    pushing changes to python and csv files to the master branch from local

---
+++ AdultIncome.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import scipy
+import csv
+import pandas as pd
+
+
+# In[2]:
+
+
+df= pd.read_csv('/Users/jermainemarshall/Downloads/adult.csv')
+
+
+# In[3]:
+
+
+df.shape
+
